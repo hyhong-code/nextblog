9e43f77527274e29d9a7e5ee98509ba507499c00
Haiyang Hong <hong961127@gmail.com>
2020-09-22 20:45:48 -0700

    Setup mongodb

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cors = requier("cors");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
 // Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: process.env.CLIENT_URL }));
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server up on port ${port}...`));
