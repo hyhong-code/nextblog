e956b3a940b6734c79efdcc8ae976706b48d1a52
Haiyang Hong <hong961127@gmail.com>
2020-09-25 06:18:10 -0700

    Add login handler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -33,8 +33,8 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    // Handle email taken
-    const { email } = req.body;
+    // Handle email not found
+    const { email, password } = req.body;
     let user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({
@@ -42,13 +42,12 @@ exports.login = async (req, res, next) => {
       });
     }
 
-    // Create user
-    const username = shortId.generate();
-    user = await User.create({
-      ...req.body,
-      username,
-      profile: `${process.env.CLIENT_URL}/profile/${username}`,
-    });
+    // Hanlde wrong password
+    if (!(await user.isCorrectPassword(password))) {
+      return res.status(401).json({
+        errors: [{ msg: `Invalid credentials.` }],
+      });
+    }
 
     // Send back a token
     const token = user.genJwtToken();
