337bfc266ef956e810d7d39aec5dc3d8ee42ee5c
Haiyang Hong <hong961127@gmail.com>
2020-10-10 21:45:53 -0700

    Add form validators

---
+++ server/controllers/form.js
@@ -0,0 +1,1 @@
+const sendGridMail = require("");
---
+++ server/utils/validators/form.js
@@ -0,0 +1,11 @@
+const { body } = require("express-validator");
+
+exports.contactFormValidator = [
+  body("name").not().isEmpty().withMessage("Name cannot be empty."),
+  body("email").isEmail().withMessage("Email is required."),
+  body("message")
+    .not()
+    .isEmpty()
+    .isLength({ min: 20 })
+    .withMessage("Message must be at least 20 characters long."),
+];
